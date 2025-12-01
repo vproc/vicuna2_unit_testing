5b5f2336cba736bba62c0d7ad80c87b376babce1
jjones <jefferson.jones@tuwien.ac.at>
2025-12-01 13:24:50 +0100

    Add basic CV32A60X integration

---
+++ build_model/verilator_support_cv32a60x.h
@@ -0,0 +1,253 @@
+// Collection of functions and variables for use with simulation with Verilator.
+// Provides standardized access to verilator simulation functions without cluttering verilator_main.cpp
+//
+// In general, accesses to internal variables (exposed with VERILATOR_PUBLIC) should be handled here by passing in a reference to TOP.  Accesses to top level interface signals (i.e. memory interfaces) should be handled by the user.
+
+#ifndef VERILATOR_SUPPORT_H
+#define VERILATOR_SUPPORT_H
+
+
+#include "Vvproc_top.h"
+#include "Vvproc_top_vproc_top.h"
+#include "Vvproc_top_cva6_pipeline__Cz1_TCz2.h"
+#include "Vvproc_top_frontend__pi1.h"
+
+//#include "Vvproc_top_vproc_top.h" --cv32e40x specific?
+//#include "Vvproc_top_cv32e40x_core__pi1.h" --cv32e40x specific?
+
+//Depeneding on the architecture selected, some of these files don't exist or have different names
+#ifdef RISCV_ZVFH
+#include "Vvproc_top_vproc_core__pi2.h"
+#include "Vvproc_top_fpu_ss_regfile.h"
+#include "Vvproc_top_fpu_ss__I1_O0_F0_FBz6_FCz7.h"
+#else
+#ifdef RISCV_ZVE32F
+#include "Vvproc_top_vproc_core__pi2.h"
+#include "Vvproc_top_fpu_ss_regfile.h"
+#include "Vvproc_top_fpu_ss__I1_O0_F0_FBz6_FCz7.h"
+#else
+#ifdef RISCV_ZVE32X
+//#include "Vvproc_top_vproc_core__pi2.h"
+#else
+#ifdef RISCV_ZFH
+#include "Vvproc_top_fpu_ss_regfile.h"
+#include "Vvproc_top_fpu_ss__I1_O0_F0_FBz3_FCz4.h"
+#else
+#ifdef RISCV_F
+#include "Vvproc_top_fpu_ss_regfile.h"
+#include "Vvproc_top_fpu_ss__I1_O0_F0_FBz3_FCz4.h"
+#endif
+#endif
+#endif
+#endif
+#endif
+
+#include "verilated.h"
+
+#include <stdio.h>
+#include <stdint.h>
+
+#ifdef TRACE_VCD
+#include "verilated_vcd_c.h"
+typedef VerilatedVcdC VerilatedTrace_t; //This file only exists if traces are enabled 
+#else
+typedef void VerilatedTrace_t;  
+#endif
+
+/*
+* Functions and Variables used to detect a stall.  Returns true if IF_PC in CV32E40X core has not changed in the provided number of cycles  TODO:CVA6 Variant
+* ARGS:
+*   - *top          - pointer to verilator top module
+*   -  max_cycles   - number of cycles after which a stall is declared
+*/
+inline uint32_t cycles_stalled = 0;
+inline uint32_t last_IF_PC = 0;
+bool check_stall(Vvproc_top *top, uint32_t max_cycles);
+
+/*
+* Function to check for a specific IF_PC.  Returns true if match.
+* ARGS:
+*   - *top          - pointer to verilator top module
+*   -  address      - address to check for
+*/
+bool check_PC(Vvproc_top *top, uint32_t address); //TODO:CVA6 Variant
+
+/*
+* Function to advance signal to the next cycle (i.e pass to after next falling edge)
+* ARGS:
+*   - *top          - pointer to verilator top module
+*/
+void advance_cycle(Vvproc_top *top);
+
+/*
+*   Function to read from memory and manage/update memory buffers.  Generalized to work on byte pointers for variable width interfaces.
+*   Queues of correct sizes are expected to be allocated and provided by the user.
+* ARGS:
+*   address        - address of the load request being issued
+*   req_valid      - validity of the load request being issued
+*   mem_w          - width of the load interface in bits
+*   mem_lat        - latency of the memory interface
+*   mem_size       - total size of the memory address space
+*
+*   *model_data_i  - pointer to memory data read interface on verilator model
+*   *model_valid_i - pointer to memory valid read interface on verilator model
+*   *model_err_i   - pointer to memory error read interface on verilator model
+*
+*   **queue_data   - pointer to data queue
+*   *queue_valid   - pointer to valid queue
+*   *queue_err     - pointer to error queue
+*
+*   *mem           - pointer to memory space
+*/
+void update_mem_load(uint32_t address, bool req_valid, uint32_t mem_w, uint32_t mem_lat, uint32_t mem_size, unsigned char *model_data_i, bool *model_valid_i, bool *model_err_i, unsigned char **queue_data, bool *queue_valid, bool *queue_err, unsigned char *mem);
+
+/*
+*   Function to write to memory.  Generalized to work on byte pointers for variable width interfaces.
+*   Values immediately written to memory.
+* ARGS:
+*   address        - address of the write request being issued
+*   req_valid      - validity of the write request being issued
+*   mem_w          - width of the write interface in bits
+*   mem_size       - total size of the memory address space
+*
+*   *model_data_o  - pointer to memory data write interface on verilator model
+*   *model_be_o    - pointer to byte enable write interface on verilator model
+*
+*   *mem           - pointer to memory space
+*/
+void update_mem_write(uint32_t address, bool req_valid, uint32_t mem_w, uint32_t mem_size, unsigned char *model_data_o, unsigned char *model_be_o, unsigned char *mem);
+
+/*
+* Check for a write to memory mapped io.  Returns true and copies written data to *data_out if a valid write occurs to the selected address
+* ARGS:
+*   address        - address of the write request being issued
+*   req_valid      - validity of the write request being issued
+*   mem_w          - width of the write interface in bits
+*
+*   *model_data_o  - pointer to memory data write interface on verilator model
+*
+*   memmap_address - address of the memory mapped io to check
+*   *data_out      - pointer to memory mapped output
+*/
+bool check_memmapio(uint32_t address, bool req_valid, uint32_t mem_w, unsigned char *model_data_o, uint32_t memmap_address, char *data_out);
+
+/*
+*   Function to setup memory.  Handles checking memory parameters, allocates main memory, and loads program.  Returns unsigned char* to main memory.  Returns NULL if error
+* ARGS:
+*   mem_sz        - size of main memory to allocate
+*   *prog_path    - program to load into memory
+*/
+unsigned char* load_program(uint32_t mem_sz, char *prog_path);
+/*
+*   Function to dump a region of memory into a file.
+* ARGS:
+*   start_addr        - start address of memory region
+*   end_addr          - end address of memory region
+*
+*   *mem              - pointer to main memory
+*
+*   *dump_path        - file path to output file
+*/
+void dump_mem_region (uint32_t start_addr, uint32_t end_addr, unsigned char *mem, char *dump_path);
+
+
+
+/*
+*   Statistics Functions.  Two main functions+sub functions.
+*
+*   update_stats() - calls all sub-functions to update statistics.  Should only be called once per simulated cycle.  Some dumping functions depend on the stats updated by this function.
+*   report_stats() - prints current state of all statistics to console.
+*/
+
+/*
+*   Cycle count update
+*/
+inline int cycles = 0;
+void update_cycles();
+
+/*
+*   Retired instruction count update
+* ARGS:
+*   - *top          - pointer to verilator top module
+*/
+inline int current_WB_PC = 0;
+inline int last_WB_PC = 0;
+inline int instr = 0;
+void update_instructions(Vvproc_top *top);  //TODO:CVA6 Variant
+/*
+*   Total Vector Instructions executed update
+* ARGS:
+*   - *top          - pointer to verilator top module
+*/
+inline int vector_instr = 0;
+void update_vector_count(Vvproc_top *top);
+
+/*
+* Average vector length calculation update.  
+* ARGS:
+*   - *top          - pointer to verilator top module
+*/
+inline int sum_vec_lengths = 0;
+inline int sum_vec_lengths_bytes = 0;
+inline float sum_vec_percentage = 0.0;
+void update_avg_vector_len(Vvproc_top *top);
+
+/*
+* Top level function to update all statistics
+* ARGS:
+*   - *top          - pointer to verilator top module
+*/
+void update_stats(Vvproc_top *top);
+
+/*
+* Report current state of all collected statistics
+*/
+void report_stats();
+
+/*
+* Update .vcd trace file. If end_cycles == 0, output entire trace.
+* ARGS:
+*   - *tfp          - pointer to verilator vcd trace object
+*   - begin_cycles  - cycle count to start trace
+*   - end_cycles    - cycle count to end trace
+*/
+void update_vcd(VerilatedTrace_t *tfp, uint32_t begin_cycles, uint32_t end_cycles);
+
+
+/*
+* Update instruction trace file. If end_cycles == 0, output entire trace.
+* ARGS:
+*   - *top          - pointer to verilator top module
+*   - *inst_trace   - pointer to .txt trace file output
+*   - begin_cycles  - cycle count to start trace
+*   - end_cycles    - cycle count to end trace
+*/
+void update_inst_trace(Vvproc_top *top, FILE *inst_trace, uint32_t begin_cycles, uint32_t end_cycles);  //TODO:CVA6 Variant
+
+/*
+* Update xreg commit log dump.  Appends any current commits to provided file.
+* ARGS:
+*   - *top          - pointer to verilator top module
+*   - *commit_log   - pointer to .txt trace file output
+*/
+void update_xreg_commit(Vvproc_top *top, FILE *commit_log); //TODO:CVA6 Variant
+
+/*
+* Update freg commit log dump.  Appends any current commits to provided file. In case RISCV_F is not enabled, remove references to fp_regfile signals as they don't exist
+* ARGS:
+*   - *top          - pointer to verilator top module
+*   - *commit_log   - pointer to .txt trace file output
+*/
+void update_freg_commit(Vvproc_top *top, FILE *commit_log); //TODO:CVA6 Variant
+
+/*
+* Update vreg commit log dump.  Appends any current commits to provided file. In case RISCV_ZVE32X is not enabled, remove references to vregfile signals as they don't exist
+* ARGS:
+*   - *top          - pointer to verilator top module
+*   - vreg_w        - width of the vector registers
+*   - *commit_log   - pointer to .txt trace file output
+*/
+void update_vreg_commit(Vvproc_top *top, int vreg_w, FILE *commit_log);
+
+
+#endif
---
+++ build_model/vproc_top_cv32a60x.sv
@@ -0,0 +1,367 @@
+// Copyright 2017-2019 ETH Zurich and University of Bologna.
+// Copyright and related rights are licensed under the Solderpad Hardware
+// License, Version 0.51 (the "License"); you may not use this file except in
+// compliance with the License.  You may obtain a copy of the License at
+// http://solderpad.org/licenses/SHL-0.51. Unless required by applicable law
+// or agreed to in writing, software, hardware and materials distributed under
+// this License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+//
+// Author: Florian Zaruba, ETH Zurich
+// Date: 19.03.2017
+// Description: CVA6 Top-level module
+
+`include "obi/typedef.svh"
+`include "obi/assign.svh"
+`include "rvfi_types.svh"
+`include "cvxif_types.svh"
+
+module vproc_top
+  import ariane_pkg::*;
+  import vproc_pkg::*;
+#(
+    // CVA6 config
+    parameter config_pkg::cva6_cfg_t CVA6Cfg = build_config_pkg::build_config(
+        cva6_config_pkg::cva6_cfg
+    ),
+
+    // CVXIF Types
+    localparam type readregflags_t = `READREGFLAGS_T(CVA6Cfg),
+    localparam type writeregflags_t = `WRITEREGFLAGS_T(CVA6Cfg),
+    localparam type id_t = `ID_T(CVA6Cfg),
+    localparam type hartid_t = `HARTID_T(CVA6Cfg),
+    localparam type x_compressed_req_t = `X_COMPRESSED_REQ_T(CVA6Cfg, hartid_t),
+    localparam type x_compressed_resp_t = `X_COMPRESSED_RESP_T(CVA6Cfg),
+    localparam type x_issue_req_t = `X_ISSUE_REQ_T(CVA6Cfg, hartit_t, id_t),
+    localparam type x_issue_resp_t = `X_ISSUE_RESP_T(CVA6Cfg, writeregflags_t, readregflags_t),
+    localparam type x_register_t = `X_REGISTER_T(CVA6Cfg, hartid_t, id_t, readregflags_t),
+    localparam type x_commit_t = `X_COMMIT_T(CVA6Cfg, hartid_t, id_t),
+    localparam type x_result_t = `X_RESULT_T(CVA6Cfg, hartid_t, id_t, writeregflags_t),
+    localparam type cvxif_req_t =
+    `CVXIF_REQ_T(CVA6Cfg, x_compressed_req_t, x_issue_req_t, x_register_req_t, x_commit_t),
+    localparam type cvxif_resp_t =
+    `CVXIF_RESP_T(CVA6Cfg, x_compressed_resp_t, x_issue_resp_t, x_result_t),
+
+    //VPROC TOP PARAMS
+    parameter int unsigned     MEM_W         = 32,  // memory bus width in bits
+    parameter int unsigned     VMEM_W        = 32,  // vector memory interface width in bits
+    parameter vreg_type        VREG_TYPE     = VREG_GENERIC,
+    parameter mul_type         MUL_TYPE      = MUL_GENERIC
+) (
+    // Subsystem Clock - SUBSYSTEM
+    input logic clk_i,
+    // Asynchronous reset active low - SUBSYSTEM
+    input logic rst_ni,
+
+    //////////////////////////////////////////
+    //vproc top signals
+    output logic               mem_req_o,
+    output logic [31:0]        mem_addr_o,
+    output logic               mem_we_o,
+    output logic [MEM_W/8-1:0] mem_be_o,
+    output logic [MEM_W  -1:0] mem_wdata_o,
+    input  logic               mem_rvalid_i,
+    input  logic               mem_err_i,
+    input  logic [MEM_W  -1:0] mem_rdata_i,
+
+    output logic               data_read_o,
+
+    output logic [31:0]        pend_vreg_wr_map_o,
+
+    output logic               mem_ireq_o,
+    output logic [31:0]        mem_iaddr_o,
+    input  logic               mem_irvalid_i,
+    input  logic               mem_ierr_i,
+    input  logic [32  -1:0]    mem_irdata_i,
+
+    output logic               data_iread_o
+    //////////////////////////////////////////
+);
+
+  //OBI FETCH
+  `OBI_LOCALPARAM_TYPE_ALL(obi_fetch, CVA6Cfg.ObiFetchbusCfg);
+  //OBI STORE
+  `OBI_LOCALPARAM_TYPE_ALL(obi_store, CVA6Cfg.ObiStorebusCfg);
+  //OBI LOAD
+  `OBI_LOCALPARAM_TYPE_ALL(obi_load, CVA6Cfg.ObiLoadbusCfg);
+
+  obi_fetch_req_t obi_fetch_req;
+  obi_fetch_rsp_t obi_fetch_rsp;
+
+  obi_store_req_t obi_store_req;
+  obi_store_rsp_t obi_store_rsp;
+
+  obi_load_req_t obi_load_req;
+  obi_load_rsp_t obi_load_rsp;
+
+  cvxif_req_t cvxif_req_o;
+  cvxif_resp_t cvxif_resp_i;
+
+  // -------------------
+  // CV32A60X Pipeline
+  // -------------------
+
+  cva6_pipeline #(
+      // CVA6 config
+      .CVA6Cfg(CVA6Cfg)
+      // RVFI PROBES
+      //.rvfi_probes_t(rvfi_probes_t)
+      //
+  ) i_cva6_pipeline (
+      .clk_i(clk_i),      
+      .rst_ni(rst_ni),    
+      .boot_addr_i('h00000080),//TODO:  CURRENTLY SET TO 80 to start at the reset vector -> could set to x8000080 to match with spike?
+      .hart_id_i('0), //Only one core
+      .irq_i(1'b0),
+      .ipi_i(1'b0),
+      .time_irq_i(1'b0),
+
+      .cvxif_req_o(cvxif_req_o),
+      .cvxif_resp_i(cvxif_resp_i),
+
+      .obi_fetch_req_o   (obi_fetch_req),
+      .obi_fetch_rsp_i   (obi_fetch_rsp),
+
+      .obi_store_req_o  (obi_store_req),
+      .obi_store_rsp_i  (obi_store_rsp),
+
+      .obi_load_req_o   (obi_load_req),
+      .obi_load_rsp_i   (obi_load_rsp)
+
+  );
+
+  // -------------------
+  // VICUNA2 Pipeline
+  // -------------------
+
+  // Vector Unit Interface
+  localparam X_NUM_RS = 3;
+  localparam X_ID_WIDTH = 4;
+  localparam X_RFR_WIDTH = 32;
+  localparam X_RFW_WIDTH = 32;
+  localparam X_MISA = 0;
+  vproc_xif #(
+      .X_NUM_RS    ( X_NUM_RS    ),
+      .X_ID_WIDTH  ( X_ID_WIDTH  ),
+      .X_MEM_WIDTH ( VMEM_W      ),
+      .X_RFR_WIDTH ( X_RFR_WIDTH ),
+      .X_RFW_WIDTH ( X_RFW_WIDTH ),
+      .X_MISA      ( X_MISA      )
+  ) vcore_xif ();
+
+  `ifdef FORCE_ALIGNED_READS
+  localparam bit [VLSU_FLAGS_W-1:0] VLSU_FLAGS = (VLSU_FLAGS_W'(1) << VLSU_ALIGNED_UNITSTRIDE);
+  `else
+  localparam bit [VLSU_FLAGS_W-1:0] VLSU_FLAGS = (VLSU_FLAGS_W'(0) << VLSU_ALIGNED_UNITSTRIDE);
+  `endif
+
+  localparam bit [BUF_FLAGS_W -1:0] BUF_FLAGS  = (BUF_FLAGS_W'(1) << BUF_DEQUEUE  ) |
+                                                  (BUF_FLAGS_W'(1) << BUF_VREG_PEND);
+
+
+`ifdef RISCV_ZVE32X
+    vproc_core #(
+        .XIF_ID_W           ( X_ID_WIDTH         ),
+        .XIF_MEM_W          ( VMEM_W             ),
+        .VREG_TYPE          ( VREG_TYPE          ),
+        .MUL_TYPE           ( MUL_TYPE           ),
+        .VLSU_FLAGS         ( VLSU_FLAGS         ),
+        .BUF_FLAGS          ( BUF_FLAGS          ),
+        .DONT_CARE_ZERO     ( 1'b0               ),
+        .ASYNC_RESET        ( 1'b0               )
+    ) v_core (
+        .clk_i              ( clk_i              ),
+        .rst_ni             ( rst_ni         ),
+
+        .xif_issue_if       ( vcore_xif          ),
+        .xif_commit_if      ( vcore_xif          ),
+        .xif_mem_if         ( vcore_xif          ), // this interface no longer exists TODO: Upgrade to OBI
+        .xif_memres_if      ( vcore_xif          ), // this interface no longer exists TODO: Upgrade to OBI
+        .xif_result_if      ( vcore_xif          ),
+        //TODO:Register Interface
+
+
+
+        .pending_load_o     ( vect_pending_load  ),
+        .pending_store_o    ( vect_pending_store ),
+
+        .csr_vtype_o        ( csr_vtype          ),
+        .csr_vl_o           ( csr_vl             ),
+        .csr_vlenb_o        ( csr_vlenb          ),
+        .csr_vstart_o       ( csr_vstart_rd      ),
+        .csr_vstart_i       ( csr_vstart_wr      ),
+        .csr_vstart_set_i   ( csr_vstart_wren    ),
+        .csr_vxrm_o         ( csr_vxrm_rd        ),
+        .csr_vxrm_i         ( csr_vxrm_wr        ),
+        .csr_vxrm_set_i     ( csr_vxrm_wren      ),
+        .csr_vxsat_o        ( csr_vxsat_rd       ),
+        .csr_vxsat_i        ( csr_vxsat_wr       ),
+        .csr_vxsat_set_i    ( csr_vxsat_wren     ),
+        `ifdef RISCV_ZVE32F
+        .fpr_wr_req_valid   ( fpr_wr_req_valid   ),
+        .fpr_wr_req_addr_o  ( fpr_wr_req_addr    ),
+        .fpr_res_valid      ( fpr_wr_resp_valid  ),
+        .float_round_mode_i ( float_round_mode   ),
+
+        .fpu_res_acc (fpu_ss_res_accepted),
+        .fpu_res_id (fpu_res_id),
+
+        `endif
+
+        .pend_vreg_wr_map_o ( pend_vreg_wr_map_o )
+    );
+
+////////////
+//Attach vicuna to CVXIF interface
+////////////
+//CV32A60X splits xif into req/response interfaces
+//CV32A60X supports most recent spec.  TODO: Upgrade Vicuna
+//    - Memory Request-Response interface is deprecated.  Currently connected directly to DMEM out port with arbitration, TODO: Upgrade to OBI
+//    - New Register Interface handles scalar source registers.  Currently re-merge into old Issue Interface (spec allows this).  TODO: Upgrade with support for this interface. -> CV32A60X "X_ISSUE_REGISTER_SPLIT = 0 : Issue and register transactions are synchronous"
+//    - Vicuna cannot currently accept any hartid.  Only one core, always tie result hartid to 0
+//    - No Result Interface error codes.  Assume traps should be handled with an external interrupt handler.  Does this mean that stalling for loads/store is no longer necessary? (have interrupt hardware keep track for precise traps)
+//Issue Interface (combined with register interface)
+
+assign vcore_xif.issue_valid = cvxif_req_o.issue_valid;
+
+assign vcore_xif.issue_req.instr = cvxif_req_o.issue_req.instr;
+assign vcore_xif.issue_req.id = cvxif_req_o.issue_req.id;
+assign vcore_xif.issue_req.mode = '0; //New issue interface does not provide privilige levels.  Vicuna shouldnt need it
+assign vcore_xif.issue_req.rs = cvxif_req_o.register.rs;
+always_comb begin
+  vcore_xif.issue_req.rs_valid = &cvxif_req_o.register.rs_valid; //one bit per source operand -> currently AND all together, require all possible scalar operands to be valid (NOT EFFICIENT)
+end
+
+//TODO: Add bit from decoder saying a register is needed -> CV32A60X doesnt actually process this bit
+assign cvxif_resp_i.issue_ready = vcore_xif.issue_ready;
+assign cvxif_resp_i.register_ready = vcore_xif.issue_ready;
+
+assign cvxif_resp_i.issue_resp.accept = vcore_xif.issue_resp.accept;
+assign cvxif_resp_i.issue_resp.writeback = vcore_xif.issue_resp.writeback; //treated as a single bit internally on CV32A60X side
+//TODO: additional bits
+
+//Commit Interface -> Appears that commit happens in the same cycle as issue?  Might not be a problem, this behavior was supported with Ibex
+
+assign vcore_xif.commit_valid = cvxif_req_o.commit_valid;
+assign vcore_xif.commit.id = cvxif_req_o.commit.id;
+assign vcore_xif.commit.commit_kill = cvxif_req_o.commit.commit_kill;
+
+//Result Interface
+
+assign vcore_xif.result_ready = cvxif_req_o.result_ready;
+
+assign cvxif_resp_i.result_valid = vcore_xif.result_valid;
+assign cvxif_resp_i.result.hartid = '0;
+assign cvxif_resp_i.result.id = vcore_xif.result.id;
+assign cvxif_resp_i.result.data = vcore_xif.result.data;
+assign cvxif_resp_i.result.rd = vcore_xif.result.rd;
+assign cvxif_resp_i.result.we = vcore_xif.result.we; //we definition seems different?
+
+
+//test signals
+logic issue_ready_test, issue_valid_test, register_ready_test, register_valid_test, commit_valid_test, result_ready_test, result_valid_test;
+assign issue_ready_test = cvxif_resp_i.issue_ready;
+assign issue_valid_test = cvxif_req_o.issue_valid;
+assign register_ready_test = cvxif_resp_i.register_ready;
+assign register_valid_test = vcore_xif.issue_req.rs_valid;
+assign commit_valid_test = cvxif_req_o.commit_valid;
+assign result_ready_test = cvxif_req_o.result_ready;
+assign result_valid_test = cvxif_resp_i.result_valid;
+logic [31:0] issue_id_test;
+assign issue_id_test=cvxif_req_o.issue_req.id;
+
+
+
+`endif
+
+
+
+  /////////
+  // Connect OBI Signals to expected VPROC_OUT interface.  TODO: Make this an OBI interface
+  ////////
+
+  //######Connect instruction memory obi signals######
+  assign mem_ireq_o = obi_fetch_req.req;
+  assign mem_iaddr_o = obi_fetch_req.a.addr;
+  //parity bits?
+  assign obi_fetch_rsp.r.rdata = mem_irdata_i;
+  assign obi_fetch_rsp.rvalid = mem_irvalid_i;
+  assign obi_fetch_rsp.r.err = mem_ierr_i;
+  assign obi_fetch_rsp.r.rid = '0; //ID? -> hart ID, hard code to 0 since only one hart in system
+  assign obi_fetch_rsp.gnt = 1'b1; //instruction memory always ready
+
+
+  //######Connect data memory obi signals######
+  //two ports (load_req and store_req).  Two requests are allowed to be issued at the same time, so arbitration must happen
+  //In case when vector unit included, additional arbitration required
+  //From documentation:
+  //    - Load only capable of read accesses
+  //    - Store exclusively for write accesses
+  //    - gntpar and rvalidpar not checked
+  //    - store interface does not use channel R
+  //    - err signal not processed (still assigned here)
+`ifndef RISCV_ZVE32X
+always_comb begin
+  mem_req_o = obi_load_req.req || obi_store_req.req;
+  obi_load_rsp.gnt = !obi_store_req.req; //core can issue load and store request at the same time.  give preference to stores
+  obi_store_rsp.gnt = 1'b1;
+
+  mem_addr_o = obi_store_req.a.addr;
+  if (obi_load_req.req && !obi_store_req.req) begin
+      mem_addr_o = obi_load_req.a.addr;
+  end
+  //only store can write
+  mem_wdata_o = obi_store_req.a.wdata; 
+  mem_we_o = obi_store_req.a.we & obi_store_req.req;
+  mem_be_o = obi_store_req.a.be; //does loading half-words change this?
+  
+  //only load uses the r interface
+  obi_load_rsp.r.rdata = mem_rdata_i;
+  obi_load_rsp.rvalid = mem_rvalid_i;
+  obi_load_rsp.r.err = mem_err_i;
+  obi_load_rsp.r.rid = '0; //ID? -> hart ID, hard code to 0 since only one hart in system
+end
+`else
+//TODO Arbitration with vector unit included.  Preference given Vector > Scalar Store > Scalar Load
+always_comb begin
+  mem_req_o = obi_load_req.req || obi_store_req.req || vcore_xif.mem_valid;
+
+  obi_load_rsp.gnt = !obi_store_req.req && !vcore_xif.mem_valid;
+  obi_store_rsp.gnt = !vcore_xif.mem_valid;
+  vcore_xif.mem_ready = 1'b1;
+
+  //default to vector interface for writes
+  mem_addr_o = vcore_xif.mem_req.addr;
+  mem_wdata_o = vcore_xif.mem_req.wdata; 
+  mem_we_o = vcore_xif.mem_req.we & vcore_xif.mem_valid;
+  mem_be_o = vcore_xif.mem_req.be; 
+
+  if (obi_store_req.req && !vcore_xif.mem_valid) begin
+      //if vector unit not using memory interface and a valid scalar store
+      mem_addr_o = obi_store_req.a.addr;
+      mem_wdata_o = obi_store_req.a.wdata; 
+      mem_we_o = obi_store_req.a.we & obi_store_req.req;
+      mem_be_o = obi_store_req.a.be; //does loading half-words change this?
+  end else if (obi_load_req.req && !obi_store_req.req && !vcore_xif.mem_valid)begin 
+      //if vector unit not using memory interface and a valid scalar load
+      mem_addr_o = obi_load_req.a.addr;
+
+  end
+  //only load uses the r interface.  can broadcast load to vector and scalar core? TODO: what happens if scalar load issued immediately after vector load?  might need to keep track of request sources or reserve port completely once vector transaction begins
+
+  obi_load_rsp.r.rdata = mem_rdata_i;
+  obi_load_rsp.rvalid = mem_rvalid_i;
+  obi_load_rsp.r.err = mem_err_i;
+  obi_load_rsp.r.rid = '0; //ID? -> hart ID, hard code to 0 since only one hart in system
+
+  vcore_xif.mem_result_valid = mem_rvalid_i;
+  vcore_xif.mem_result.rdata = mem_rdata_i;
+  vcore_xif.mem_result.err = mem_err_i;
+end
+
+`endif
+
+ 
+
+endmodule  // ariane
