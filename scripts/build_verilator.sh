2ec4b40322bc1810a70bf6590b76ec2b9f4c20df
jjones <jefferson.jones@tuwien.ac.at>
2026-01-23 12:25:25 +0100

    use most recent version of verilator

--- scripts/build_verilator.sh
+++ scripts/build_verilator.sh
@@ -10,7 +10,7 @@ git clone https://github.com/verilator/verilator
 unset VERILATOR_ROOT
 cd verilator
 #git checkout tags/v4.210 # unique_ptr issue with new Ubuntu 24.  known issue, Verilator suggestion is to upgrade
-git checkout tags/v5.030
+#git checkout tags/v5.030
 autoconf
 ./configure --prefix $INSTALL_PATH
 make -j8
