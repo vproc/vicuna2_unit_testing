37eea85a286e7b233265404d71684040316ad389
Daniel Blattner <daniel.blattner@student.tuwien.ac.at>
2026-02-20 11:37:03 +0100

    Added Zvbb and Zvbc to build file

--- build_model/vector_config.cmake
+++ build_model/vector_config.cmake
@@ -3,8 +3,8 @@
 ###
 
 #Currently Supported: rv32im, rv32im_zve32x, rv32imf, rv32imf_zhf, rv32imf_zve32x, rv32imf_zve32f
-set(RISCV_ARCH rv32im_zve32x CACHE STRING "Specify the configuration")
+set(RISCV_ARCH rv32im_zve32x_zvbb_zvbc CACHE STRING "Specify the configuration")
 
 set(VREG_W 128)
 set(VMEM_W 32)
-set(VPROC_PIPELINES "${VMEM_W}:VLSU 64:VELEM,VSLD,VDIV,VALU,VMUL")
+set(VPROC_PIPELINES "${VMEM_W}:VLSU 64:VELEM,VSLD,VDIV,VALU,VMUL,VZVBB,VZVBC")
