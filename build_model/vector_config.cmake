ab93325487251eeb3b210667d3429910cf4a9664
jjones <jefferson.jones@tuwien.ac.at>
2026-03-24 15:06:38 +0100

    revert to original configuration and dont cache RISCV_ARCH variable

--- build_model/vector_config.cmake
+++ build_model/vector_config.cmake
@@ -3,7 +3,7 @@
 ###
 
 #Currently Supported: rv32im, rv32im_zve32x, rv32imf, rv32imf_zhf, rv32imf_zve32x, rv32imf_zve32f, rv32im_zve32x_zvbb, rv32im_zve32x_zvbc, rv32im_zve32x_zvbb_zvbc
-set(RISCV_ARCH rv32im_zve32x CACHE STRING "Specify the configuration")
+set(RISCV_ARCH "rv32im_zve32x")
 
 #Currently Supported: cv32e40x, cv32a60x
 set(SCALAR_CORE "cv32e40x")
@@ -11,4 +11,4 @@ set(SCALAR_CORE "cv32e40x")
 
 set(VMEM_W 32)
 set(VREG_W 128)
-set(VPROC_PIPELINES "${VMEM_W}:VLSU 64:VELEM,VSLD,VDIV,VALU,VMUL,VZVBB,VZVBC")
+set(VPROC_PIPELINES "${VMEM_W}:VLSU 32:VELEM,VSLD,VDIV,VALU,VMUL")
